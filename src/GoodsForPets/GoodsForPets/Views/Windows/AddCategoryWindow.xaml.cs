76d3ab00fed3c147a968ca6be818eb0fc96ce897
Denis Denisov <prostolink200@gmail.com>
2022-10-25 21:16:34 +0300

    Added window for adding the product category

---
+++ src/GoodsForPets/GoodsForPets/Views/Windows/AddCategoryWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+
+using GoodsForPets.Models;
+
+namespace GoodsForPets.Views.Windows
+{
+    /// <summary>
+    /// Interaction logic for AddCategoryWindow.xaml
+    /// </summary>
+    public partial class AddCategoryWindow : Window
+    {
+        private Context _context;
+        public AddCategoryWindow()
+        {
+            InitializeComponent();
+            _context = new Context();
+        }
+
+        private async void AddNewCategoryButton_Click(object sender, RoutedEventArgs e)
+        {
+            Category category = new Category
+            {
+                Name = NameTextBox.Text,
+                Unit = UnitTextBox.Text
+            };
+            _context.Categories.Add(category);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
