b8f9e9afe021f5f55528661b9d6119ecf5867863
Denis Denisov <prostolink200@gmail.com>
2022-10-25 21:15:51 +0300

    The possibility of a full-fledged addition / editing of the product was implemented

--- src/GoodsForPets/GoodsForPets/Views/Windows/ProductWindow.xaml.cs
+++ src/GoodsForPets/GoodsForPets/Views/Windows/ProductWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Windows;
@@ -6,6 +7,7 @@ using System.Windows.Media.Imaging;
 
 using GoodsForPets.Models;
 
+using Microsoft.EntityFrameworkCore;
 using Microsoft.VisualBasic.FileIO;
 using Microsoft.Win32;
 
@@ -27,8 +29,12 @@ namespace GoodsForPets.Views.Windows
             _context = new Context();
         }
 
-        private void ProductWindow_Loaded(object sender, RoutedEventArgs e)
+        private async void ProductWindow_Loaded(object sender, RoutedEventArgs e)
         {
+            CategoryComboBox.ItemsSource = await _context.Categories.ToListAsync();
+            ManufacturerComboBox.ItemsSource = await _context.Manufacturers.ToListAsync();
+            SupplierComboBox.ItemsSource = await _context.Suppliers.ToListAsync();
+
             if (_isEdit)
             {
                 Title = "Редактирование товара";
@@ -45,6 +51,9 @@ namespace GoodsForPets.Views.Windows
                     QuantityInStockTextBox.Text = _product.QuantityInStock.ToString();
                     DescriptionTextBlock.Text = _product.Description;
                     ProductImage.Source = new BitmapImage(new Uri(Environment.CurrentDirectory + $"\\Resources\\ItemsImages\\{(_product.Photo == null ? "emptyItem.png" : _product.Photo)}", UriKind.Absolute));
+                    SupplierComboBox.SelectedIndex = _context.Suppliers.First(s => s.Name == _product.Supplier).Id - 1;
+                    ManufacturerComboBox.SelectedIndex = _context.Manufacturers.First(m => m.Name == _product.Manufacturer).Id - 1;
+                    CategoryComboBox.SelectedIndex = _context.Categories.First(c => c.Name == _product.Category).Id - 1;
                 }
             }
             else
@@ -52,22 +61,57 @@ namespace GoodsForPets.Views.Windows
                 Title = "Добавление товара";
                 SaveCreateProductButton.Content = "Добавить";
                 DeleteProductButton.Visibility = Visibility.Hidden;
+                (SupplierComboBox.ItemsSource as List<Supplier>)?.Insert(0, new Supplier { Id = 0, Name = "Не выбран" });
+                (ManufacturerComboBox.ItemsSource as List<Manufacturer>)?.Insert(0, new Manufacturer { Id = 0, Name = "Не выбран" });
+                (CategoryComboBox.ItemsSource as List<Category>)?.Insert(0, new Category { Id = 0, Name = "Не выбрана", Unit = "Нет" });
+                SupplierComboBox.SelectedIndex = 0;
+                ManufacturerComboBox.SelectedIndex = 0;
+                CategoryComboBox.SelectedIndex = 0;
+            }
+        }
+
+        private async void ReloadComboBoxes()
+        {
+            CategoryComboBox.ItemsSource = await _context.Categories.ToListAsync();
+            ManufacturerComboBox.ItemsSource = await _context.Manufacturers.ToListAsync();
+            SupplierComboBox.ItemsSource = await _context.Suppliers.ToListAsync();
+
+            if (_isEdit)
+            {
+                SupplierComboBox.SelectedIndex = _context.Suppliers.First(s => s.Name == _product.Supplier).Id - 1;
+                ManufacturerComboBox.SelectedIndex = _context.Manufacturers.First(m => m.Name == _product.Manufacturer).Id - 1;
+                CategoryComboBox.SelectedIndex = _context.Categories.First(c => c.Name == _product.Category).Id - 1;
+            }
+            else
+            {
+                (SupplierComboBox.ItemsSource as List<Supplier>)?.Insert(0, new Supplier { Id = 0, Name = "Не выбран" });
+                (ManufacturerComboBox.ItemsSource as List<Manufacturer>)?.Insert(0, new Manufacturer { Id = 0, Name = "Не выбран" });
+                (CategoryComboBox.ItemsSource as List<Category>)?.Insert(0, new Category { Id = 0, Name = "Не выбрана", Unit = "Нет" });
+                SupplierComboBox.SelectedIndex = 0;
+                ManufacturerComboBox.SelectedIndex = 0;
+                CategoryComboBox.SelectedIndex = 0;
             }
         }
 
         private void AddSupplierButton_Click(object sender, RoutedEventArgs e)
         {
-
+            AddSupplierWindow addSupplierWindow = new AddSupplierWindow();
+            addSupplierWindow.ShowDialog();
+            ReloadComboBoxes();
         }
 
         private void AddManufacturerButton_Click(object sender, RoutedEventArgs e)
         {
-
+            AddManufacturerWindow addManufacturerWindow = new AddManufacturerWindow();
+            addManufacturerWindow.ShowDialog();
+            ReloadComboBoxes();
         }
 
         private void AddCategoryButton_Click(object sender, RoutedEventArgs e)
         {
-
+            AddCategoryWindow addCategoryWindow = new AddCategoryWindow();
+            addCategoryWindow.ShowDialog();
+            ReloadComboBoxes();
         }
 
         private void AddProductImageButton_Click(object sender, RoutedEventArgs e)
@@ -117,12 +161,45 @@ namespace GoodsForPets.Views.Windows
             }
             else
             {
-                Product product = new Product
+                if (SupplierComboBox.SelectedIndex != 0
+                    && ManufacturerComboBox.SelectedIndex != 0
+                    && CategoryComboBox.SelectedIndex != 0
+                    && !string.IsNullOrWhiteSpace(ArticleNumerTextBox.Text)
+                    && !string.IsNullOrWhiteSpace(NameTextBox.Text)
+                    && !string.IsNullOrWhiteSpace(CostTextBox.Text)
+                    && !string.IsNullOrWhiteSpace(MaxDiscountAmountTextBox.Text)
+                    && !string.IsNullOrWhiteSpace(CurrentDiscountAmountTextBox.Text)
+                    && !string.IsNullOrWhiteSpace(QuantityInStockTextBox.Text)
+                    && !string.IsNullOrWhiteSpace(DescriptionTextBlock.Text))
                 {
+                    ProductInfo productInfo = new ProductInfo
+                    {
+                        CategoryId = _context.Categories.First(c => c.Name == CategoryComboBox.SelectedValue.ToString()).Id,
+                        ManufacturerId = _context.Manufacturers.First(m => m.Name == ManufacturerComboBox.SelectedValue.ToString()).Id,
+                        SupplierId = _context.Suppliers.First(s => s.Name == SupplierComboBox.SelectedValue.ToString()).Id
+                    };
+                    _context.ProductsInfo.Add(productInfo);
+                    await _context.SaveChangesAsync();
 
-                };
-                products.Add(product);
-                await _context.SaveChangesAsync();
+                    Product product = new Product
+                    {
+                        Id = ArticleNumerTextBox.Text,
+                        Name = NameTextBox.Text,
+                        Cost = Convert.ToDouble(CostTextBox.Text),
+                        MaxDiscountAmount = Convert.ToInt32(MaxDiscountAmountTextBox.Text),
+                        CurrentDiscountAmount = Convert.ToInt32(CurrentDiscountAmountTextBox.Text),
+                        QuantityInStock = Convert.ToInt32(QuantityInStockTextBox.Text),
+                        Description = DescriptionTextBlock.Text,
+                        ProductInfoId = productInfo.Id
+                    };
+                    _context.Products.Add(product);
+                    await _context.SaveChangesAsync();
+                    MessageBox.Show("Товар успешно добавлен!", "Уведомление", MessageBoxButton.YesNo, MessageBoxImage.Question);
+                }
+                else
+                {
+                    MessageBox.Show("Все поля обязательны к заполнению!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
             }
         }
     }
