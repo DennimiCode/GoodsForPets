ed50f952b1bda2d3e4dc925ec772be3daca994fc
Denis Denisov <prostolink200@gmail.com>
2022-10-25 21:17:03 +0300

    Added window for adding the product manufacturer

---
+++ src/GoodsForPets/GoodsForPets/Views/Windows/AddManufacturerWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+
+using GoodsForPets.Models;
+
+namespace GoodsForPets.Views.Windows
+{
+    /// <summary>
+    /// Interaction logic for AddManufacturerWindow.xaml
+    /// </summary>
+    public partial class AddManufacturerWindow : Window
+    {
+        private Context _context;
+        public AddManufacturerWindow()
+        {
+            InitializeComponent();
+            _context = new Context();
+        }
+
+        private async void AddNewManufacturerButton_Click(object sender, RoutedEventArgs e)
+        {
+            Manufacturer manufacturer = new Manufacturer
+            {
+                Name = NameTextBox.Text
+            };
+            _context.Manufacturers.Add(manufacturer);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
