021104c44561ff58afc318951f4e04ee7402128c
Denis Denisov <prostolink200@gmail.com>
2022-10-25 21:17:22 +0300

    Added window for adding the product supplier

---
+++ src/GoodsForPets/GoodsForPets/Views/Windows/AddSupplierWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+
+using GoodsForPets.Models;
+
+namespace GoodsForPets.Views.Windows
+{
+    /// <summary>
+    /// Interaction logic for AddSupplierWindow.xaml
+    /// </summary>
+    public partial class AddSupplierWindow : Window
+    {
+        private Context _context;
+        public AddSupplierWindow()
+        {
+            InitializeComponent();
+            _context = new Context();
+        }
+
+        private async void AddNewSupplierButton_Click(object sender, RoutedEventArgs e)
+        {
+            Supplier supplier = new Supplier
+            {
+                Name = NameTextBox.Text
+            };
+            _context.Suppliers.Add(supplier);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
